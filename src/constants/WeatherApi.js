76010417ebd17c5c888ceadcee21cee5e732a44f
Abdul Qawi Kamran <129019043+abdulqawikamran@users.noreply.github.com>
2023-12-03 23:14:58 +0800

    query for location
    todo:
    handle no location
    handle inputname === jsonName
    UI update when loading

--- src/constants/WeatherApi.js
+++ src/constants/WeatherApi.js
@@ -1,6 +1,7 @@
 const WeatherApi = {
     API_KEY: 'ff138df005044387bfc2597bae7fa93f', 
-    PREFIX: 'https://api.openweathermap.org/data/2.5/weather?lat='
+    WEATHER_PREFIX: 'https://api.openweathermap.org/data/2.5/weather?lat=',
+    GEOCODE_PREFFIX: 'http://api.openweathermap.org/geo/1.0/direct?q=', 
 }
 
 export default  WeatherApi; 
--- src/pages/weather/Weather.js
+++ src/pages/weather/Weather.js
@@ -1,22 +1,70 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import WeatherModel from "../../models/WeatherModel";
 import WeatherApi from "../../constants/WeatherApi";
 
 function Weather() {
   const [weather, setWeather] = useState(null);
-  const { API_KEY, PREFIX } = WeatherApi;
+  const { API_KEY, WEATHER_PREFIX, GEOCODE_PREFFIX } = WeatherApi;
   const lat = 4.75;
   const long = 115.0;
-  const [inputtedLocation, setInputtedLocation] = useState('');
+  const [inputtedLocation, setInputtedLocation] = useState("");
+  const locationFetchLimit = "5";
+
+  const submitLocation = () => {
+    const formattedLocation = inputtedLocation
+      .toLowerCase()
+      .split(" ")
+      .join("-");
+    const url = `${GEOCODE_PREFFIX}${formattedLocation}&limit=${locationFetchLimit}&appid=${API_KEY}`;
+    var alreadyHasLocation = false;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.find((location) => {
+          if (location.country === "MY" && alreadyHasLocation === false) {
+            console.log(
+              location.name,
+              "lat: ",
+              location.lat,
+              "long: ",
+              location.lon
+            );
+            getWeather(location.lat, location.lon);
+          }
+        });
+      });
+  };
+
+  const getWeather = (lat, lon) => {
+    const url = `${WEATHER_PREFIX}${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${API_KEY}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("json is", JSON.stringify(data, null, 2));
+        const currentWeather = new WeatherModel(
+          data.weather[0].main,
+          data.weather[0].description,
+          data.main.temp,
+          data.main.feels_like,
+          data.main.temp_min,
+          data.main.temp_max,
+          data.sys.sunrise,
+          data.sys.sunset,
+          data.name
+        );
+        setWeather(currentWeather); 
+      });
+  };
 
   useEffect(() => {
-    // console.log(`API_KEY is ${API_KEY}, prefix is ${PREFIX}`)
     /*
     just temporary, later use Geocoding API
     lat: 4.75000000
     long: 115.00000000
     */
-    const url = `${PREFIX}${lat}&lon=${long}&exclude=minutely&units=metric&appid=${API_KEY}`;
+
+    //TODO: refactor get the location into its own function
+    const url = `${WEATHER_PREFIX}${lat}&lon=${long}&exclude=minutely&units=metric&appid=${API_KEY}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
@@ -39,29 +87,39 @@ function Weather() {
 
   const WeatherDisplay = () => {
     return (
-        <div>
-            <p>Main: {weather.main}</p>
-            <p>Description: {weather.description}</p>
-            <p>Current Temperature: {weather.temp}°</p>
-            <p>Feels Like: {weather.feels_like}</p>
-            <p>Minimum Temperature: {weather.tempMin}</p>
-            <p>Maximum Temperature: {weather.tempMax}</p>
-            <p>Sunrise: {weather.sunrise}</p>
-            <p>Sunset: {weather.sunset}</p>
-        </div>
-    ); 
-  }
+      <div>
+        <p>Main: {weather.main}</p>
+        <p>Description: {weather.description}</p>
+        <p>Current Temperature: {weather.temp}°</p>
+        <p>Minimum Temperature: {weather.tempMin}</p>
+        <p>Maximum Temperature: {weather.tempMax}</p>
+        <p>Sunrise: {weather.sunrise}</p>
+        <p>Sunset: {weather.sunset}</p>
+      </div>
+    );
+  };
 
   const SearchLocation = () => {
+    const inputRef = useRef();
+
+    const handleInput = (event) => {
+      setInputtedLocation(event.target.value);
+    };
+
+    useEffect(() => {
+      if (inputRef.current) {
+        inputRef.current.focus(null);
+      }
+    }, []);
 
     return (
-        <div>
-            <h2>Enter your location</h2>
-            <input type='text' value={inputtedLocation}  />
-            <button>Find out Weather</button>
-        </div>
-    ); 
-  }
+      <div>
+        <h2>Enter your location</h2>
+        <input value={inputtedLocation} onChange={handleInput} ref={inputRef} />
+        <button onClick={submitLocation}>Find out Weather</button>
+      </div>
+    );
+  };
 
   return (
     <div>
@@ -76,7 +134,6 @@ function Weather() {
 }
 
 export default Weather;
-
 
 /*
 flow 
