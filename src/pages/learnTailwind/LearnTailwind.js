08ce91736ecd050e97b86b646fe8c5ae8f116c1d
Abdul Qawi Kamran <qawitherev@gmail.com>
2023-12-09 14:27:22 +0800

    added responsive gridlayout

--- src/pages/learnTailwind/LearnTailwind.js
+++ src/pages/learnTailwind/LearnTailwind.js
@@ -4,22 +4,81 @@ export function LearnTailWind() {
   return (
     <>
       <h1>Using Grid</h1>
-      <div className="grid auto-rows-[192px] grid-cols-6 gap-4 p-10">
+      <div className="grid auto-rows-[200px] sm:grid-cols-1 md:grid-cols-6 gap-4 p-20 md:p-10">
         {[...Array(7)].map((_, i) => (
           <div
             key={i}
             className={`rounded-xl border-2 border-slate-400 bg-neutral-100 dark:bg-neutral-400
-            ${i === 0 ? " col-span-3 row-span-1 " : ""}
-          ${i === 1 ? "col-span-1 row-span-1" : ""}
-          ${[2, 5].includes(i) ? " col-span-2 row-span-2 " : ""}
-          ${i === 3 ? " col-span-1 row-span-2 " : ""}
-          ${i === 4 ? " col-span-3 row-span-2 " : ""}
-          ${i === 6 ? " col-span-4 row-span-1 " : ""}
+            ${i === 0 ? "md:col-span-3 md:row-span-1" : ""}
+          ${i === 1 ? "md:col-span-1 md:row-span-1" : ""}
+          ${[2, 5].includes(i) ? "md:col-span-2 md:row-span-2" : ""}
+          ${i === 3 ? "md:col-span-1 md:row-span-2" : ""}
+          ${i === 4 ? "md:col-span-3 md:row-span-2" : ""}
+          ${i === 6 ? "md:col-span-4 md:row-span-1" : ""}
+          transform transition-transform hover:scale-95
           `}
           >
-            {i}
+            <SubBento data={i} />
           </div>
         ))}
+      </div>
+    </>
+  );
+}
+
+function SubBento(props) {
+  const data = props.data;
+  switch (data) {
+    case 0:
+      return (
+        <>
+          <SubBento0 />
+        </>
+      );
+    case 1:
+      return (
+        <>
+          <h1>{props.data}</h1>
+        </>
+      );
+    case 2:
+      return (
+        <>
+          <h1>{props.data}</h1>
+        </>
+      );
+    case 3:
+      return (
+        <>
+          <h1>{props.data}</h1>
+        </>
+      );
+    case 4:
+      return (
+        <>
+          <h1>{props.data}</h1>
+        </>
+      );
+    case 5:
+      return (
+        <>
+          <h1>{props.data}</h1>
+        </>
+      );
+    case 6:
+      return (
+        <>
+          <h1>{props.data}</h1>
+        </>
+      );
+  }
+}
+
+function SubBento0() {
+  return (
+    <>
+      <div>
+        <h1>This is SubBento0</h1>
       </div>
     </>
   );
